506c50182c6b523a4e34a78f1eec69f944efe2e5
Brennan Cheung <git@brennancheung.com>
2019-07-02 23:07:49 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Typography variant="h1">Find Accountability</Typography>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A site to find accountability partners and keep track of your progress.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
